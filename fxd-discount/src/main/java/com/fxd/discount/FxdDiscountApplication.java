fb33904062681cb3b56ff681a60dc378a4b56588
zfz <1009600394@qq.com>
2021-04-20 09:44:28 +0800

    重新更新nacos注册中心， 增加服务提供者provider和消费者consumer测试项目 把之前的不用的注册中心引用的包更换为nacos的包

---
+++ fxd-discount/src/main/java/com/fxd/discount/FxdDiscountApplication.java
@@ -0,0 +1,11 @@
+package com.fxd.discount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxdDiscountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FxdDiscountApplication.class,args);
+    }
+}
